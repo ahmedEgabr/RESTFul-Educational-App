eafd84a6dee0df02a6191bb1f37fcfc694e02c38
muhammed.atif <muhammed.atif@amenli.com>
2022-07-25 02:13:04 +0200

    update lecture form in site admin

--- courses/admin_forms/lecture.py
+++ courses/admin_forms/lecture.py
@@ -1,14 +1,20 @@
 from dal import autocomplete
 from django import forms
-from courses.models import Lecture
+from courses.models import Lecture, Course
 from categories.models import ReferenceCategory
 
 class LectureForm(forms.ModelForm):
     reference_category = forms.ModelChoiceField(queryset=ReferenceCategory.objects.all(),
                                     disabled=False, required=False, label="Reference category")
+
+    course = forms.ModelChoiceField(
+    queryset=Course.objects.all(), required=False, label="Course Topics Filter"
+    )
+
     class Meta:
         model = Lecture
         fields = (
+        'course',
         'topic',
         'title',
         'description',
@@ -26,5 +32,9 @@ class LectureForm(forms.ModelForm):
             'references': autocomplete.ModelSelect2Multiple(
             url='courses:references-autocomplete',
             forward=['reference_category']
+            ),
+            'topic': autocomplete.ModelSelect2(
+            url='courses:topic-autocomplete',
+            forward=['course']
             )
         }
