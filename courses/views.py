176471eea91d0d5941cb7b5781b2d6b41ecdf346
muhammed.atif <muhammed.atif@amenli.com>
2022-07-29 00:05:37 +0200

    fix a bug in autocomplete topics filter

--- courses/views.py
+++ courses/views.py
@@ -17,7 +17,11 @@ class TopicAutocomplete(StafUserRequiredMixin, autocomplete.Select2QuerySetView)
         course = self.forwarded.get('course', None)
         qs = None
         if course:
-            qs = Topic.objects.filter(unit__course__id=course, created_by=self.request.user)
+            if self.request.user.is_superuser:
+                qs = Topic.objects.filter(unit__course__id=course)
+            else:
+                qs = Topic.objects.filter(unit__course__id=course, created_by=self.request.user)
+
             if self.q:
                 qs = qs.filter(title__icontains=self.q)
         return qs
