280486d72e47678121dd82b557468039ffc6942b
muhammed.atif <muhammed.atif@amenli.com>
2022-07-30 01:51:08 +0200

    Reset Attachement and add Abstract Attachement

--- courses/models/abstract_attachement.py
+++ courses/models/abstract_attachement.py
@@ -5,3 +5,6 @@ from main.models import UserActionModel, TimeStampedModel
 class Attachement(UserActionModel, TimeStampedModel):
     title = models.CharField(max_length=100)
     file = models.FileField(upload_to='attachements')
+
+    class Meta:
+        abstract = True
