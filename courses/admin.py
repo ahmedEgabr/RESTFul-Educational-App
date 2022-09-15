9b4cefa06e2be4cb16c5c5df701167312f0ee53e
muhammed.atif <muhammed.atif@amenli.com>
2022-09-15 22:36:50 +0200

    fix course privacy

--- courses/admin.py
+++ courses/admin.py
@@ -169,10 +169,10 @@ class CoursePrivacyInline(nested_admin.NestedStackedInline):
     'available_from',
     'enrollment_duration',
     'enrollment_duration_type',
-    'is_downloadable',
-    'is_downloadable_for_enrolled_users_only',
-    'is_quiz_available',
-    'is_quiz_available_for_enrolled_users_only',
+    # 'is_downloadable',
+    # 'is_downloadable_for_enrolled_users_only',
+    # 'is_quiz_available',
+    # 'is_quiz_available_for_enrolled_users_only',
     'is_attachements_available',
     "is_attachements_available_for_enrolled_users_only"
     )
